c24055bde6d59bc1bfcff3ba2ecb3d04a83eb211
ahmed.essidmu <ahmed.essid@apeiron-tech.com>
2022-07-09 15:18:05 +0100

    - Add Sign in screen -- Minor fixes

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import WelcomeScreen from './lib/screens/WelcomeScreen';
+import WelcomeScreen from './lib/screens/welcome/WelcomeScreen';
+import SignInScreen from './lib/screens/signIn/SignInScreen';
+import { COLORS } from './lib/constants/colors';
 
 const Stack = createNativeStackNavigator();
 
 const screenOptions = {
   headerShown: false,
+  contentStyle: {
+    backgroundColor: COLORS.white,
+  },
 };
 
 export default function App(){
@@ -16,6 +21,10 @@ export default function App(){
         <Stack.Screen
           name="Welcome"
           component={WelcomeScreen}
+        />
+        <Stack.Screen
+          name="SignIn"
+          component={SignInScreen}
         />
       </Stack.Navigator>
     </NavigationContainer>
