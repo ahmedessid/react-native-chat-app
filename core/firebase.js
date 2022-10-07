9b59cfa23a383d2195da142759f53e9c6b8857d6
ahmed.essid <ahmed.essid@apeiron-tech.com>
2022-10-07 22:37:12 +0100

    -Send Image message from device (ImagePicker) -- Send message from both sides (same chat room) -- Minor fixes

--- core/firebase.js
+++ core/firebase.js
@@ -2,6 +2,8 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,5 +21,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
 const auth = getAuth(app);
+const storage = getStorage();
 
-export {firestore, auth};
+export {firestore, auth, storage};
